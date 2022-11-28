375ebb2482ffbdd17431b76e4949e9ed76f7356e
dangkat <89470874+dangkat@users.noreply.github.com>
2022-11-28 13:31:30 -0500

    Update hw3_ML.R

--- hw3_ML.R
+++ hw3_ML.R
@@ -38,16 +38,13 @@ library(iml)
 train <- read.csv("C:/Users/kat4538/Documents/MSA/FALL 3/machine learning/hw 3/insurance_t.csv")
 valid <- read.csv("C:/Users/kat4538/Documents/MSA/FALL 3/machine learning/hw 3/insurance_v.csv")
 
-# DETERMINE TYPE OF VARIABLES
-str(train)
-# var <- sapply(train, n_distinct) 
-
-##### check which variables have missing #####
+####################### MISSING VALUE IMPUTATION ###########################
+# check which variables have missing values
 gg_miss_var(train) # 14 variables
 na_count <-sapply(train, function(y) sum(length(which(is.na(y)))))
 na_count <- data.frame(na_count)
 
-##### missing value imputation for TRAINING DATA #####
+## TRAINING DATA ##
 #convert all categorical var to factors
 col_names <- names(train)[c(2,7:8,12,14,18,20,22,24,26:27,29:30,36,38)]
 train[,col_names] <- lapply(train[,col_names] , factor)
@@ -112,78 +109,7 @@ train$MMCRED <- as.character(train$MMCRED)
 train$MMCRED[which(train$MMCRED > 2)] <- "3+" # new category for 3+ money market credits
 table(train$INS, train$MMCRED)
 
-##### NNET #####
-# standardizing continuous variables
-train2 <- train %>% mutate(across(where(is.numeric), scale))
-valid2 <- valid %>% mutate(across(where(is.numeric), scale))
-
-# build model
-train2$INS <- as.factor(train2$INS)
-valid2$INS <- as.factor(valid2$INS)
-
-set.seed(444)
-nn <- nnet(INS ~ ., data = train2, size = 5)
-
-# plot network
-plotnet(nn)
-
-# tuning parameters
-tune_grid <- expand.grid(
-  .size = c(3, 4, 5, 6, 7),
-  .decay = c(0, 0.5, 1)
-)
-set.seed(444)
-nn.caret <- train(INS~., data = train2,
-                  method = "nnet", # Neural network using the nnet package
-                  tuneGrid = tune_grid,
-                  trControl = trainControl(method = 'cv', number = 10),
-                  trace = FALSE)
-
-nn.caret$bestTune
-
-# final nnet
-set.seed(444)
-nn <- nnet(INS ~ ., data = train2,
-            size = 3, decay = 1)
-
-plotnet(nn)
-
-# Hinton Diagram
-nn_weights <- matrix(data = nn$wts[1:217], ncol = 3, nrow = 72)
-rownames(nn_weights) <- c("bias", nn$coefnames)
-colnames(nn_weights) <- c("h1", "h2", "h3")
-
-ggplot(melt(nn_weights), aes(x=Var1, y=Var2, size=abs(value), 
-                             color=as.factor(sign(value)))) +
-  geom_point(shape = 15) +
-  scale_size_area(max_size = 40) +
-  labs(x = "", y = "", title = "Hinton Diagram of NN Weights") +
-  theme_bw()
-
-##### NAIVE BAYES #####
-set.seed(444)
-train$INS <- as.factor(train$INS)
-nb <- naiveBayes(INS ~ ., data = train, 
-                 laplace = 0.1, usekernel = TRUE)
-
-
-# Optimize laplace and kernel - CARET ONLY ABLE TO TUNE CLASSIFICATION PROBLEMS FOR NAIVE BAYES
-# tune_grid <- expand.grid(
-#   usekernel = c(TRUE),
-#   fL = c(0, 0.5, 1),
-#   adjust=c(0,0.5,1.0))
-# 
-# set.seed(444)
-# nb.caret <- train(x, y, data = train,
-#                        method = "nb", 
-#                        tuneGrid = tune_grid,
-#                        trControl = trainControl(method = 'cv', number = 10))
-# nb.caret$bestTune
-# 
-# nb <- naiveBayes(INS ~ ., data = train, 
-#                  fL = 0.5, usekernel = TRUE, adjust = 0)
-
-##### missing value imputation for VALIDATION DATA #####
+## VALIDATION DATA ##
 valid$INS <- as.factor(valid$INS)
 
 #convert all categorical var to factors
@@ -250,14 +176,50 @@ valid$MMCRED <- as.character(valid$MMCRED)
 valid$MMCRED[which(valid$MMCRED > 2)] <- "3+" # new category for 3+ money market credits
 table(valid$INS, valid$MMCRED)
 
-#### ROC CURVE ON VALIDATION DATA ####
-# NNET
+############## NEW MODELS ###############
+
+##### NNET #####
+# standardizing continuous variables
+train2 <- train %>% mutate(across(where(is.numeric), scale))
+valid2 <- valid %>% mutate(across(where(is.numeric), scale))
+
+# build model
+train2$INS <- as.factor(train2$INS)
+valid2$INS <- as.factor(valid2$INS)
+
+set.seed(444)
+nn <- nnet(INS ~ ., data = train2, size = 5)
+
+# plot network
+plotnet(nn)
+
+# tuning parameters
+tune_grid <- expand.grid(
+  .size = c(3, 4, 5, 6, 7),
+  .decay = c(0, 0.5, 1)
+)
+set.seed(444)
+nn.caret <- train(INS~., data = train2,
+                  method = "nnet", # Neural network using the nnet package
+                  tuneGrid = tune_grid,
+                  trControl = trainControl(method = 'cv', number = 10),
+                  trace = FALSE)
+
+nn.caret$bestTune
+
+# final nnet
+set.seed(444)
+nn <- nnet(INS ~ ., data = train2,
+            size = 3, decay = 1)
+
+plotnet(nn)
+
+# ROC curve
 valid_p <- valid2
 valid_p$p_hat <- predict(nn, newdata=valid_p, type = "raw")[,1]
 p1nn <- valid_p$p_hat[valid_p$INS == 1]
 p0nn <- valid_p$p_hat[valid_p$INS == 0]
 
-#ROC curve
 pred.nn <- prediction(valid_p$p_hat, factor(valid_p$INS)) 
 perf.nn <- performance(pred.nn, measure = "tpr", x.measure = "fpr")
 plot(perf.nn, lwd = 3, col = "dodgerblue3", main = paste0("Neural Net ROC Plot (AUC = ", round(AUROC(valid_p$INS, valid_p$p_hat), 3),")"), 
@@ -278,10 +240,32 @@ ggplot(valid_p, aes(p_hat, fill = factor(INS))) +
   scale_fill_manual(values = c("#1C86EE", "#FFB52E"),name = "Customer Decision", labels = c("Not Bought", "Bought")) +
   theme(plot.title = element_text(hjust = 0.5), plot.subtitle =element_text(hjust = 0.5) )
 
+##### NAIVE BAYES #####
+set.seed(444)
+train$INS <- as.factor(train$INS)
+nb <- naiveBayes(INS ~ ., data = train, 
+                 laplace = 0.1, usekernel = TRUE)
 
-# NAIVE BAYES
+
+# Optimize laplace and kernel - CARET ONLY ABLE TO TUNE CLASSIFICATION PROBLEMS FOR NAIVE BAYES
+tune_grid <- expand.grid(
+  usekernel = c(TRUE),
+  fL = c(0, 0.5, 1),
+  adjust=c(0,0.5,1.0))
+
+set.seed(444)
+nb.caret <- train(x, y, data = train,
+                       method = "nb",
+                       tuneGrid = tune_grid,
+                       trControl = trainControl(method = 'cv', number = 10))
+nb.caret$bestTune
+
+nb <- naiveBayes(INS ~ ., data = train,
+                 fL = 0.5, usekernel = TRUE, adjust = 0)
+
+# ROC curve
 val_p <- valid
-val_p$p_hat <- predict(nb, type = "raw", newdata = val_p)[,1]
+val_p$p_hat <- predict(nb, type = "raw", newdata = val_p)[,2]
 p1nb <- val_p$p_hat[val_p$INS == 1]
 p0nb <- val_p$p_hat[val_p$INS == 0]
 
@@ -292,16 +276,51 @@ plot(perf.nn, lwd = 3, col = "dodgerblue3", main = paste0("Naive Bayes ROC Plot 
      ylab = "True Positive")
 abline(a = 0, b = 1, lty = 3)
 
+################## RANDOM FOREST AS BEST MODEL ##################
+train$INS <- as.factor(train$INS)
+set.seed(444)
+rf <- randomForest(INS ~ . - LORES - FLAG_NA_CRSCORE - FLAG_NA_INCOME
+                   - FLAG_NA_ACCTAGE - SDB - NSFAMT - INAREA, 
+                   data = train, ntree = 200, mtry = 8, importance = TRUE)
 
-# coefficient of discrimination
-coef_discrim <- mean(p1nb) - mean(p1nb)
-ggplot(val_p, aes(p_hat, fill = factor(INS))) +
-  geom_density(alpha = 0.7) +
-  labs(x = "Predicted Probability",
-       y = "Density",
-       fill = "Outcome",
-       title = "Discrimination Slope for Naive Bayes",
-       subtitle = paste("Coefficient of Discrimination = ",
-                        round(coef_discrim, 3), sep = "")) +
-  scale_fill_manual(values = c("#1C86EE", "#FFB52E"),name = "Customer Decision", labels = c("Not Bought", "Bought")) +
-  theme(plot.title = element_text(hjust = 0.5), plot.subtitle =element_text(hjust = 0.5) )
+varImpPlot(rf,
+           sort = TRUE,
+           n.var = 10)
+rfimp2 <- importance(rf)
+rfimp2 <- as.data.frame(rfimp2)
+write.csv(rfimp2, "C:/Users/kat4538/Documents/MSA/FALL 3/machine learning/hw 3/rfimp.csv")
+
+# ROC curve
+valid_p2 <- valid
+valid_p2$p_hat <- predict(rf, type = "prob", newdata = valid_p2)[,2]
+
+p1 <- valid_p2$p_hat[valid_p2$INS == 1]
+p0 <- valid_p2$p_hat[valid_p2$INS == 0]
+
+pred.rf <- prediction(valid_p2$p_hat, factor(valid_p2$INS)) 
+perf.rf <- performance(pred.rf, measure = "tpr", x.measure = "fpr")
+plot(perf.rf, lwd = 3, col = "dodgerblue3", main = paste0("Random Forest ROC Plot (AUC = ", round(AUROC(valid_p2$INS, valid_p2$p_hat), 3),")"), 
+     xlab = "False Positive",
+     ylab = "True Positive")
+abline(a = 0, b = 1, lty = 3)
+
+######################## INTERPRETATIONS USING RF #######################
+# PDP
+pd_plot <- FeatureEffects$new(pred.rf, method = "pdp")
+pd_plot$plot(c("age"))
+pd_plot$plot()
+
+# PDP on ICE
+pdice_plot <- FeatureEffects$new(pred.rf, method = "pdp+ice")
+pdice_plot$plot(c("age"))
+pdice_plot$plot()
+
+# ALE 
+ale_plot <- FeatureEffects$new(pred.rf, method = "ale")
+ale_plot$plot(c("age"))
+ale_plot$plot()
+
+# Shapley Values 
+point <- 732
+shap <- Shapley$new(pred.rf, x.interest = training[point,-1])
+shap$plot()
